cfd1153cb6a8dab64be4430762696266c77d8a2f
GianbaBotassi <gianba89@gmail.com>
2023-10-18 14:35:55 +0200

    Forgot equal into for condition

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -2,7 +2,7 @@ package org.lessons.java;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i <= 100; i++) {
             if(i % 3 == 0 && i % 5 == 0){
                 System.out.println("FizzBuzz");
             }else if(i%3 == 0){
